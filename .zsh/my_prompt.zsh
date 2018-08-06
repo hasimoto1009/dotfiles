34475f0382f15c9373fc206b78514055491c4b0e
motty1009 <hasimoto1009@gmail.com>
2018-08-06 20:55:24 +0900

    [zsh] プロンプトの変更

--- .zsh/my_prompt.zsh
+++ .zsh/my_prompt.zsh
@@ -1,16 +1,15 @@
-PROMPT="%(?.%{${fg[green]}%}.%{${fg[red]}%})[%n@%m]%{${reset_color}%}[%~] %# "
-
 zstyle ':vcs_info:*' formats '%F{green}(%s)[%b]%f'
 zstyle ':vcs_info:*' actionformats '%F{red}(%s)[%b|%a]%f'
 zstyle ':vcs_info:git:*' check-for-change true
 zstyle ':vcs_info:git:*' stagedstr "+"
 zstyle ':vcs_info:git:*' unstagedstr "-"
-zstyle ':vcs_info:git:*' formats '(%s)-[%b] %c%u'
-zstyle ':vcs_info:git:*' actionformats '(%s)-[%b|%a] %c%u'
+zstyle ':vcs_info:git:*' formats '[%b] %c%u'
+zstyle ':vcs_info:git:*' actionformats '[%b|%a] %c%u'
 
 function _update_vcs_info_msg() {
   LANG=en_US.UTF-8 vcs_info
-  RPROMPT="${vcs_info_msg_0_}"
+  PROMPT="%{$fg[yellow]%}┏━ %{$reset_color%}[%n]%{${fg[cyan]}%}[%~]%{${reset_color}%}%{${fg[yellow]}%}${vcs_info_msg_0_}%{${reset_color}%}
+%{$fg[yellow]%}┗━ %{$reset_color%}%(?.%{${fg[green]}%}.%{${fg[red]}%})%#%{${reset_color}%} "
 }
 add-zsh-hook precmd _update_vcs_info_msg
 
