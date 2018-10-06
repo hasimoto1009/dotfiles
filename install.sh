fb3523c632d53e499b75583412331f058e610c00
hasimoto1009 <hasimoto1009@gmail.com>
2018-10-06 17:46:35 +0000

    remove dot from file name

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 DOTFILES_DIR=$HOME/dotfiles
-ln -sf $DOTFILES_DIR/.zshenv $HOME/.zshenv
-ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES_DIR/zshenv $HOME/.zshenv
+ln -sf $DOTFILES_DIR/zshrc $HOME/.zshrc
+ln -sf $DOTFILES_DIR/gemrc $HOME/.gemrc
+ln -sf $DOTFILES_DIR/gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/gitignore_global $HOME/.gitignore_global
+ln -sf $DOTFILES_DIR/gvimrc $HOME/.gvimrc
+ln -sf $DOTFILES_DIR/ideavimrc $HOME/.ideavimrc
+ln -sf $DOTFILES_DIR/railsrc $HOME/.railsrc
+ln -sf $DOTFILES_DIR/tigrc $HOME/.tigrc
+ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
 ln -sfn $DOTFILES_DIR/.zsh $HOME/.zsh
-ln -sf $DOTFILES_DIR/.gemrc $HOME/.gemrc
-ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
-ln -sf $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
-ln -sf $DOTFILES_DIR/.gvimrc $HOME/.gvimrc
-ln -sf $DOTFILES_DIR/.ideavimrc $HOME/.ideavimrc
-ln -sf $DOTFILES_DIR/.railsrc $HOME/.railsrc
-ln -sf $DOTFILES_DIR/.tigrc $HOME/.tigrc
-ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
 
 create_dir() {
 if [ ! -d $1 ]; then
