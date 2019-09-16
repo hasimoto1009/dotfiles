0bc6dc04dd29d8149088fcede45b2d8d8c040f54
hasimoto1009 <hasimoto1009@gmail.com>
2019-09-16 18:26:28 +0900

    [bundle] create bundle config

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+create_dir() {
+if [ ! -d $1 ]; then
+    mkdir -p $1
+fi
+}
+create_dir ~/.bundle/
+create_dir ~/.vim/undo
+create_dir ~/.vim/swap
+create_dir ~/.vim/backup
+
 DOTFILES_DIR=$HOME/dotfiles
 ln -sf $DOTFILES_DIR/zshenv $HOME/.zshenv
 ln -sf $DOTFILES_DIR/zshrc $HOME/.zshrc
@@ -11,17 +22,8 @@ ln -sf $DOTFILES_DIR/railsrc $HOME/.railsrc
 ln -sf $DOTFILES_DIR/tigrc $HOME/.tigrc
 ln -sf $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
 ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
+ln -sf $DOTFILES_DIR/bundle_config $HOME/.bundle/config
 ln -sfn $DOTFILES_DIR/.zsh $HOME/.zsh
-
-create_dir() {
-if [ ! -d $1 ]; then
-    mkdir -p $1
-fi
-}
-
-create_dir ~/.vim/undo
-create_dir ~/.vim/swap
-create_dir ~/.vim/backup
 
 # dein.vim install
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && sh ./installer.sh ~/.vim/dein
