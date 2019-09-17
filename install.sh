52c649018b7875ed2a55c8f47be9d851d42860df
hasimoto1009 <hasimoto1009@gmail.com>
2019-09-17 12:47:36 +0900

    [install]add cargo

--- install.sh
+++ install.sh
@@ -30,6 +30,10 @@ ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
 ln -sf $DOTFILES_DIR/bundle_config $HOME/.bundle/config
 ln -sfn $DOTFILES_DIR/.zsh $HOME/.zsh
 
+if !(type "cargo" > /dev/null 2>&1); then
+  curl https://sh.rustup.rs -sSf | sh
+fi
+
 # dein.vim install
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && sh ./installer.sh ~/.vim/dein
 
