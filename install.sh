5ca0cf29f1850e8aef8dbd2a6cde2c6beb2b0950
hasimoto1009 <hasimoto1009@gmail.com>
2019-10-16 14:06:29 +0900

    [vim] remove gvimrc

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ ln -sf $DOTFILES_DIR/zshrc $HOME/.zshrc
 ln -sf $DOTFILES_DIR/gemrc $HOME/.gemrc
 ln -sf $DOTFILES_DIR/gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/gitignore_global $HOME/.gitignore_global
-ln -sf $DOTFILES_DIR/gvimrc $HOME/.gvimrc
 ln -sf $DOTFILES_DIR/ideavimrc $HOME/.ideavimrc
 ln -sf $DOTFILES_DIR/railsrc $HOME/.railsrc
 ln -sf $DOTFILES_DIR/ripgreprc $HOME/.ripgreprc
