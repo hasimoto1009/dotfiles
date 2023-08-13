c9418754ad1e50c4b67c91dc88a7279037b4a990
hasimoto1009 <hasimoto1009@gmail.com>
2023-08-13 20:22:31 +0900

    [install.sh] refactor

--- install.sh
+++ install.sh
@@ -3,50 +3,36 @@
 echo 'export ZDOTDIR=$HOME/.config/zsh' > $HOME/.zshenv
 
 if !(type "brew" > /dev/null 2>&1); then
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew install readline # irbで日本語入力をするために必要
-if !(type "git" > /dev/null 2>&1); then
-  brew install git
+brew install git      # 最新版をインストール
+
+if !(type "diff-highlight" > /dev/null 2>&1); then
+  ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
 fi
 
 if !(type "rbenv" > /dev/null 2>&1); then
   curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 fi
 
-create_dir() {
-if [ ! -d $1 ]; then
-    mkdir -p $1
+if [ ! -x "`which nodenv`" ]; then
+  curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-installer | bash
 fi
-}
-create_dir ~/.vim/undo
-create_dir ~/.vim/swap
-create_dir ~/.vim/backup
+
+if !(type "cargo" > /dev/null 2>&1); then
+  curl https://sh.rustup.rs -sSf | sh
+fi
 
 if [ ! -x "`which fzf`" ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 fi
 
-DOTFILES_DIR=$HOME/dotfiles
-ln -sf $DOTFILES_DIR/ideavimrc $HOME/.ideavimrc
-ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
+# vim init settings
+ln -sf $HOME/.config/vimrc $HOME/.vimrc
+mkdir -p  ~/.vim/undo && mkdir -p  ~/.vim/swap && mkdir -p  ~/.vim/backup
 
-if !(type "cargo" > /dev/null 2>&1); then
-  curl https://sh.rustup.rs -sSf | sh
-fi
-
-if !(type "diff-highlight" > /dev/null 2>&1); then
-  ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
-fi
-
-# dein.vim install
-#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && sh ./installer.sh ~/.vim/dein
-
-
-if [ ! -x "`which nodenv`" ]; then
-  curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-installer | bash
-fi
-
+# minpac install
 git clone --depth 1 https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
